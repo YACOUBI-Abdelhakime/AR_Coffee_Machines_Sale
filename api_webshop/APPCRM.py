f49b0d70f18f5b029a9c355803160307e0347fe8
younesk8 <36771709+younesk8@users.noreply.github.com>
2023-03-04 15:52:14 +0000

    API GENERATE QR CODE AND SEND BY EMAIL

--- api_webshop/APPCRM.py
+++ api_webshop/APPCRM.py
@@ -1,28 +1,59 @@
-from fastapi import FastAPI, HTTPException, Depends, Header
-import jwt
-from typing import List
+from fastapi import FastAPI, HTTPException
+import requests
+import qrcode
+import io
+import base64
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.image import MIMEImage
 
 app = FastAPI()
-
-# Secret key for encoding and decoding JWT tokens
-SECRET_KEY = "mysecretkey"
 
 # Define the endpoint of the CRM's API
 crm_endpoint = "https://crm.example.com/api/v1"
 
-# Function for checking if the user has the correct role
-def check_roles(role: str = Header(None)):
-    if role not in ["admin", "user"]:
-        raise HTTPException(status_code=400, detail="Invalid role")
-    return role
+def generate_qr_code(unique_id: str):
+    # Generate QR code
+    qr = qrcode.QRCode(version=1, box_size=10, border=5)
+    qr.add_data(unique_id)
+    qr.make(fit=True)
+    qr_image = qr.make_image(fill_color="black", back_color="white")
 
-# Function for decoding JWT tokens
-def decode_token(token: str):
-    try:
-        payload = jwt.decode(token, SECRET_KEY)
-    except jwt.InvalidTokenError:
-        raise HTTPException(status_code=400, detail="Invalid token")
-    return payload
+    # Convert image to base64 string
+    with io.BytesIO() as buffer:
+        qr_image.save(buffer, format="PNG")
+        qr_image_base64 = base64.b64encode(buffer.getvalue()).decode()
+
+    return qr_image_base64
+
+@app.get("/send_qr_code/{user_id}")
+async def send_qr_code(user_id: str):
+    # Retrieve user from CRM
+    user = await get_data_from_crm(f"/users/{user_id}")
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    # Generate QR code
+    qr_code = generate_qr_code(user_id)
+
+    # Send email with QR code
+    sender_email = "your_email@example.com"
+    receiver_email = user["email"]
+    message = MIMEMultipart()
+    message["From"] = sender_email
+    message["To"] = receiver_email
+    message["Subject"] = "Your QR code"
+    text = MIMEText("Please use this QR code to authenticate in the mobile app.")
+    message.attach(text)
+    image = MIMEImage(base64.b64decode(qr_code))
+    message.attach(image)
+    with smtplib.SMTP("smtp.gmail.com", 587) as server:
+        server.starttls()
+        server.login(sender_email, "your_password")
+        server.sendmail(sender_email, receiver_email, message.as_string())
+
+    return {"message": "QR code sent successfully"}
 
 # Function for sending GET requests to the CRM's API
 async def get_data_from_crm(path: str):
@@ -58,50 +89,27 @@ async def delete_data_from_crm(path: str):
 
 # Define the routes for the API
 @app.get("/customers/{customer_id}")
-async def read_customer(customer_id: int, auth_token: str = Header(None)):
-    # Decode the JWT token to get the user's role
-    payload = decode_token(auth_token)
-    user_role = payload["role"]
-# Check if the user has the correct role
-    if user_role != "admin":
-        raise HTTPException(status_code=403, detail="Forbidden")
+async def read_customer(customer_id: int):
     # Retrieve the customer from the CRM
     customer = await get_data_from_crm(f"/customers/{customer_id}")
     if customer is None:
         raise HTTPException(status_code=404, detail="Customer not found")
     return customer
+
 @app.post("/customers/")
-async def create_customer(customer: dict, auth_token: str = Header(None), role: str = Depends(check_roles)):
-    # Decode the JWT token to get the user's role
-    payload = decode_token(auth_token)
-    user_role = payload["role"]
-    # Check if the user has the correct role
-    if user_role != role:
-        raise HTTPException(status_code=403, detail="Forbidden")
+async def create_customer(customer: dict):
     # Create a new customer in the CRM
     new_customer = await post_data_to_crm("/customers/", customer)
     return {"id": new_customer["id"]}
 
 @app.put("/customers/{customer_id}")
-async def update_customer(customer_id: int, customer: dict, auth_token: str = Header(None)):
-    # Decode the JWT token to get the user's role
-    payload = decode_token(auth_token)
-    user_role = payload["role"]
-    # Check if the user has the correct role
-    if user_role != "admin":
-        raise HTTPException(status_code=403, detail="Forbidden")
+async def update_customer(customer_id: int, customer: dict):
     # Update the customer in the CRM
     await put_data_to_crm(f"/customers/{customer_id}", customer)
     return {"id": customer_id}
 
 @app.delete("/customers/{customer_id}")
-async def delete_customer(customer_id: int, auth_token: str = Header(None)):
-    # Decode the JWT token to get the user's role
-    payload = decode_token(auth_token)
-    user_role = payload["role"]
-    # Check if the user has the correct role
-    if user_role != "admin":
-        raise HTTPException(status_code=403, detail="Forbidden")
+async def delete_customer(customer_id: int):
     # Delete the customer from the CRM
     await delete_data_from_crm(f"/customers/{customer_id}")
     return {"id": customer_id}
